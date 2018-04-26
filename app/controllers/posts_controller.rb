37b1d6dc26b930c4d56ac34a57fb116fdeb00f86
kazoo <kazuki_saito@diveintocode.jp>
2018-04-26 21:50:33 +0900

    twiclone letter_opener実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,6 +31,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
+	    PostMailer.post_mail(@post).deliver
         format.html { redirect_to @post, notice: 'はいできました' }
         format.json { render :show, status: :created, location: @post }
       else
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
   resources :favorites, only: [:create, :destroy]
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
