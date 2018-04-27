83485c1f5de2da0300b0b14029a5316919e33a10
kazoo <kazuki_saito@diveintocode.jp>
2018-04-27 19:47:01 +0900

    twiclone ImageUploader実装1.2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,7 +31,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new
+    @post = Post.new(post_params)
 	@post.user_id = current_user.id 
 
 	@post.image.retrieve_from_cache! params[:cache][:image]
